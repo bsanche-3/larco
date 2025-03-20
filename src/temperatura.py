195aca924f65403dce3c46cfb4d2daf5f326d5af
Brian Sanchez <bsanche3@hotmail.com>
2025-03-19 19:19:09 -0500

    Se realiza la ejecucion del filtro por rango de fechas

--- src/temperatura.py
+++ src/temperatura.py
@@ -30,7 +30,8 @@ def load_data():
         print(f"⚠️ El archivo {file_path} NO existe. Verifica la ruta.")
     df = pd.read_csv(file_path)
     return df
-df = load_data()
+
+data = load_data()
 
 
 # 3. Implementación de la Barra de Navegación
@@ -42,8 +43,38 @@ menu = st.sidebar.radio(
 # 4. Mostrar los Datos
 if menu == "Datos":
     st.subheader("📂 Datos Generados")
-    st.dataframe(df, height=500, use_container_width=True)
+    st.dataframe(data, height=500, use_container_width=True)
+
+# 5. Filtrar por Fecha
+data["DATE"] = pd.to_datetime(data["DATE"], format="%d/%m/%Y", dayfirst=True)
+filtered_data = data  # Asegurar que filtered_data esté definido en todo el script
+if menu == "Visualización":
+    st.subheader("📅 Filtrar por Fecha")
+
+    # Convertir las fechas a tipo date
+    min_date = data["DATE"].min().date()
+    max_date = data["DATE"].max().date()
+
+    fecha_inicio, fecha_fin = st.sidebar.date_input(
+        "Selecciona el rango de fechas:",
+        [min_date, max_date],
+        min_value=min_date,
+        max_value=max_date
+    )
+
+    # Convertir fecha_inicio y fecha_fin a datetime para la comparación
+    fecha_inicio = pd.to_datetime(fecha_inicio)
+    fecha_fin = pd.to_datetime(fecha_fin)
+
+    filtered_data = filtered_data[
+        (filtered_data["DATE"] >= fecha_inicio) & (filtered_data["DATE"] <= fecha_fin)
+    ]
+    
+     # 8. Botón para Reiniciar Filtros
+    if st.sidebar.button("Reiniciar Filtros"):
+        filtered_data = data
+        st.rerun()
 
 # 11. Ejecución del Script
 if __name__ == "__main__":
-    st.sidebar.info("Ejecuta este script con: streamlit run talento-roadmap-app.py")
+    st.sidebar.info("Ejecuta este script con: streamlit run d:/Usuario/Descargas/larco/src/temperatura.py")
